a7c07e8ff18706ceb4871bb6bdc7073c8d6b4792
Usman Chadhar <musmanchaddar@gmail.com>
2026-06-30 16:29:32 +0500

    Initial Commit

--- Numpy.py
+++ Numpy.py
@@ -1,3 +1,21 @@
 import numpy as np 
+
+#trunc
 arr = np.trunc([-2.2343, 3.6667])
 print(arr)
+
+#fix
+arr1 = np.fix([-2.2343, 3.6667])
+print(arr1)
+
+#Rounding
+arr2 = np.around([3.6666, 2])
+print(arr2)
+
+#Floor
+arr3 = np.floor([3.666, 2])
+print(arr3)
+
+#Ceil
+arr4 = np.ceil([-1.166, 2.1])
+print(arr4)

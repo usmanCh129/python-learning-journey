1ddb347fedcff393be00242836464e5aa3945197
Usman Chadhar <musmanchaddar@gmail.com>
2026-07-26 10:29:03 +0500

    Triangulation

--- SciPy.py
+++ SciPy.py
@@ -1,13 +1,14 @@
-import numpy as np
-from scipy.sparse.csgraph import floyd_warshall
-from scipy.sparse import csr_matrix
-
-arr = np.array([
-  [0, 1, 2],
-  [1, 0, 0],
-  [2, 0, 0]
-])
-
-newarr = csr_matrix(arr)
-
-print(floyd_warshall(newarr, return_predecessors=True))
+import numpy as np 
+from scipy.spatial import Delaunay 
+import matplotlib.pyplot as plt 
+points = np.array([ 
+  [2, 4],
+  [3, 4],
+  [3, 0],
+  [2, 2],
+  [4, 1]
+ ])
+simplices = Delaunay(points).simplices 
+plt.triplot(points[:, 0], points[:, 0], simplices)
+plt.scatter(points [:, 0], points[:, 1], color = 'r')
+plt.show()

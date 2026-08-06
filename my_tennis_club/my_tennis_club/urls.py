dd07eba7d8bf79fd0af42c690e9aa55be30b1bae
Usman <musmanchaddar@gmail.com>
2026-08-06 22:19:33 +0500

    Refactor project: delete tennis club folder and update files

--- my_tennis_club/my_tennis_club/urls.py
+++ my_tennis_club/my_tennis_club/urls.py
@@ -2,7 +2,7 @@
 URL configuration for my_tennis_club project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/6.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('members.urls')),
     path('admin/', admin.site.urls),
 ]

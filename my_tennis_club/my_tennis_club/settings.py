89c613fd6fec434fbe03888131df555634f95c81
Usman <musmanchaddar@gmail.com>
2026-08-24 10:57:15 +0500

    Learning Python, Djano Costimizing page with CSS

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -136,3 +136,12 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = BASE_DIR / 'productionfiles'
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'productionfiles'
+
+STATIC_URL = 'static/'
+
+#Add this in your settings.py file:
+STATICFILES_DIRS = [
+    BASE_DIR / 'mystaticfiles'
+]
